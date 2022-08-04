cfe3a1d67ed588ca9c2f08f814ec357148f3f77b
Michael Osgood <michael.f.osgood@gmail.com>
2022-08-04 08:30:54 -0700

    added paycom

--- assets/javascript/app.js
+++
@@ -1,8 +0,0 @@
-
-// Function is triggered when user clicks on button from index.html
-// Random quote from the array displays on the UI
-function newQuote() {
-    var randomNumber = Math.floor(Math.random() * (quotes.length));
-    console.log("Random Number: " + randomNumber);
-    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
-}
